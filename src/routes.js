51ebbb4e0e44dd05329d3003e6c668db93b74787
messiasGeovani <messias.geovani.lima@gmail.com>
2020-01-22 16:56:30 +0000

    routes file configuration

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+/**
+ * Routes file module
+ */
+
+//  express module
+const Express = require('express')
+
+// router config
+const routes = Express.Router()
+
+/**
+ * Creaing the routes
+ */
+
+//  clients creation route
+routes.post('/client', () => {
+    console.log('A client was created')
+})
+
+//  transactions creation route
+routes.post('/transaction', () => {
+    console.log('A transaction was created')
+})
+
+//  clients recovery route
+routes.get('/clients', () => {
+    console.log('The clients was retrieved')
+})
+
+//  transactions recovery route
+routes.get('/transactions', () => {
+    console.log('The transactions was retrieved')
+})
